c904c50fd9253f28c9e0de5d57cebac6414531ea
markkong30 <markkong30@gmail.com>
2022-09-30 00:23:12 +0100

    change linkdein address

--- frontend/src/components/SocialMedia.jsx
+++ frontend/src/components/SocialMedia.jsx
@@ -4,25 +4,25 @@ import { FaFacebookF } from 'react-icons/fa';
 import { MdEmail } from 'react-icons/md';
 
 const SocialMedia = () => {
-  return (
-    <div className='app__social'>
-      <div>
-        <a href="https://github.com/markkong30" target="_blank">
-          <BsGithub />
-        </a>
-      </div>
-      <div>
-        <a href="mailto:markkong30@gmail.com" target="_blank">
-          <MdEmail />
-        </a>
-      </div>
-      <div>
-        <a href="https://www.linkedin.com/in/yuen-kei-kong-810981225/" target="_blank">
-          <BsLinkedin />
-        </a>
-      </div>
-    </div>
-  );
+	return (
+		<div className="app__social">
+			<div>
+				<a href="https://github.com/markkong30" target="_blank">
+					<BsGithub />
+				</a>
+			</div>
+			<div>
+				<a href="mailto:markkong30@gmail.com" target="_blank">
+					<MdEmail />
+				</a>
+			</div>
+			<div>
+				<a href="https://www.linkedin.com/in/mark-kong-810981225/" target="_blank">
+					<BsLinkedin />
+				</a>
+			</div>
+		</div>
+	);
 };
 
 export default SocialMedia;
